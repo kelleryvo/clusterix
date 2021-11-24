919ee2aeb97a6a1346823619054520262d05eaab
Yvo Keller <keller.yvo@gmail.com>
2021-11-24 22:15:49 +0100

    update requirements.txt to make it work in 2021

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from .routes import main, results, projections
 
 # App init
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 app.register_blueprint(main)
 app.register_blueprint(results)
 app.register_blueprint(projections)
